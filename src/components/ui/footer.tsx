a2b8277904e4b87bc2fcf0fa77285451175bbf73
haashirnawaz9 <153224457+haashirnawaz9@users.noreply.github.com>
2025-07-03 13:40:45 -0400

    Your commit message here

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function Footer() {
   return (
     <footer className="flex flex-row justify-center items-center text-center h-[60px] bg-gradient-to-r from-blue-300 to-indigo-400">
-      <h1 className="font-normal text-sm">Copyright © 2024 HashBot AI | All Rights Reserved</h1>
+      <h1 className="font-normal text-sm">Copyright © 2025 HashBot AI | All Rights Reserved</h1>
       <Link href="https://www.instagram.com/haashir_nawaz_/">
       </Link>
     </footer>
